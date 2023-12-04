a6d035b10b06c845907578b956c66467eed8223e
tanaka ken <k.tanaka.sys22@morijyobi.ac.jp>
2023-12-04 14:24:05 +0900

    演習8

--- scripts/my_scene.js
+++ scripts/my_scene.js
@@ -19,23 +19,42 @@ class MyScene extends Phaser.Scene {
 
     // シーン初期化処理
     create() {
+        //タイマー
+        this._timeCounter = 0;  
+        //残り時間
+        this._leftTime = 3;
+        this.countdounTimer = true;
          // 単体画像をシーンに追加(X座標,Y座標,画像名)
         this.add.image(D_WIDTH/2, D_HEIGHT/2, 'background');
         this.text1 = this.add.text(10, 10, 'Scene 1').setFontSize(32).setColor('#ff0');
         this.text2 = this.add.text(600, 400, 'MyWorld').setFontSize(32).setColor('#ff0');
         const player1 = this.physics.add.sprite(400, 200, 'taro');
         this.player1=player1;
-        const player2 = this.physics.add.sprite(500, 150, 'jiro');
-        this.player2 = player2;
+        // const player2 = this.physics.add.sprite(500, 150, 'jiro');
+        // this.player2 = player2;
         
         //this.player.angle=0;
         ///WASDキーを検知できるようにする
+
+        let physics = this.physics.add.group();// 動く物体をまとめる
+        let  randx = Phaser.Math.Between(200, 400);
+        let  randy = Phaser.Math.Between(100, 200);
+        physics.create(randx, randy, 'hanako');
 
         this.keys = {};
         this.keys.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
         this.keys.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         this.keys.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
         this.keys.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+
+         // hanakoに衝突したら実行する
+         this.physics.add.overlap(player1, physics, collectHanako, null, this);
+         function collectHanako(p,hanako){  
+            // 物理演算(ボディ)の無効化 (非アクティブ化, 非表示)
+             this.text3 = this.add.text(100, 150, '痛い!').setFontSize(32).setColor('#ff0');
+             hanako.disableBody(false, true);
+ 
+         }
     }
 
     wasd_move(keys,object){
@@ -45,16 +64,44 @@ class MyScene extends Phaser.Scene {
             this.object = this.add.text(100, 50, 'Hello!').setFontSize(32).setColor('#ff0');
         }else if(keys.keyD.isDown){ //Dが押されている時
             object.destroy();
-        }else if(keys.keyW.isDown){
-            let  randx = Phaser.Math.Between(100, 400); 
-            this.physics.add.sprite(randx, 100, 'hanako');;
+        // }else if(keys.keyW.isDown){
+        //     let  randx = Phaser.Math.Between(100, 400); 
+        //     this.physics.add.sprite(randx, 100, 'hanako');
         }
 
     }
     
+    countdown(delta){
+        // 毎フレーム事にタイマーを更新
+        this._timeCounter += delta;
+        // _timeCounterが1000になった1秒
+        if(this._timeCounter > 1000) {
+            // 1000ミリ秒経過したのでカウンターをリセット
+            this._timeCounter = 0;
+            // 残り時間を減らす
+            this._leftTime --;
+        }
+        if(this._leftTime <= 0) {
+            this.quitHanako();
 
+        }
+
+    }
+  // 花子の表示処理
+
+  quitHanako(){
+    let  randx = Phaser.Math.Between(200, 400);
+    let  randy = Phaser.Math.Between(100, 200);
+    this.physics.add.sprite(randx, randy, 'hanako');
+
+
+    //カウントダウンタイマーを止めるためにフラグをfalseにする
+    this.countdounTimer = false;
+    return;
+
+}
   // 毎フレーム実行される繰り返し処理
-    update() {
+    update(time, delta) {
         //  if (this.player.x >= D_WIDTH - 100) this.player_direction = -1;
         //  if (this.player.x <= 100) this.player_direction = 1;
         //   プレイヤーの移動
@@ -81,13 +128,18 @@ class MyScene extends Phaser.Scene {
        // キーボードの情報を取得
        let cursors = this.input.keyboard.createCursorKeys();
        if (cursors.left.isDown) {
-       this.player1.x -= 5;// 左方向に移動
-       this.player2.x += 5;// 左方向に移動
+        this.player1.setVelocityX(-30);
+       //this.player2.x += 5;// 左方向に移動
        } else if (cursors.right.isDown) {
-       this.player1.x += 5;// 右方向に移動
-       this.player2.x -= 5;// 右方向に移動
-       }
+        this.player1.setVelocityX(30);
+       //this.player2.x -= 5;// 右方向に移動
+       }else if (cursors.up.isDown) {
+        this.player1.setVelocityY(-30);
+        }else if (cursors.down.isDown) {
+            this.player1.setVelocityY(30);
+        }
        this.wasd_move(this.keys,this.text2);
+       if(this.countdounTimer) this.countdown(delta);
     }
 
 }
